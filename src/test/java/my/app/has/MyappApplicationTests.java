13327507ad8197861b4933e3481b15abf7f30b38
Hoa <danghuyhoa@gmail.com>
2019-10-27 23:59:13 +0700

    Init project

---
+++ src/test/java/my/app/has/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package my.app.has;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

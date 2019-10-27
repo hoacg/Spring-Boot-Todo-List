050d1f96ff0072589f25395bc8896b2c71b5218b
Hoa <danghuyhoa@gmail.com>
2019-10-28 00:09:23 +0700

    Init for deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/myapp-1.0.jar
---
+++ src/main/java/my/app/has/MyApplication.java
@@ -0,0 +1,22 @@
+package my.app.has;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String index() {
+        return "index";
+    }
+
+}

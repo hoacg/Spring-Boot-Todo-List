ca267b2c958e590950b669f64f3e2738930a536c
Hoa <danghuyhoa@gmail.com>
2019-10-29 22:21:17 +0700

    [improve] remove redundant line

--- src/main/java/my/app/has/todolist/repositories/ICategoryRepository.java
+++ src/main/java/my/app/has/todolist/repositories/ICategoryRepository.java
@@ -2,7 +2,6 @@ package my.app.has.todolist.repositories;
 
 import my.app.has.todolist.models.Category;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface ICategoryRepository extends CrudRepository<Category, Long> {
 }

45cbfd6bc3e27fd06c51ec71989e20be7ef28b93
Hoa <danghuyhoa@gmail.com>
2019-10-29 22:11:46 +0700

    [new] add api for get all todos in a category

--- src/main/java/my/app/has/todolist/services/impl/TodoService.java
+++ src/main/java/my/app/has/todolist/services/impl/TodoService.java
@@ -1,9 +1,9 @@
 package my.app.has.todolist.services.impl;
 
+import my.app.has.todolist.models.Category;
 import my.app.has.todolist.models.Todo;
 import my.app.has.todolist.repositories.ITodoRepository;
 import my.app.has.todolist.services.ITodoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,21 +11,26 @@ import java.util.Optional;
 @Service
 public class TodoService implements ITodoService {
 
-    @Autowired
-    ITodoRepository todoRepository;
+    private ITodoRepository todoRepository;
+
+    TodoService(ITodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     public Iterable<Todo> getTodoList() {
         return todoRepository.findAll();
     }
 
     @Override
+    public Iterable<Todo> getTodoByCategory(Category category) {
+        return todoRepository.findAllByCategory(category);
+    }
+
+    @Override
     public Todo getTodoDetail(Long id) {
         Optional<Todo> optional = todoRepository.findById(id);
 
-        if (optional.isPresent()) {
-            return optional.get();
-        }
-        return null;
+        return optional.orElse(null);
     }
 
     @Override

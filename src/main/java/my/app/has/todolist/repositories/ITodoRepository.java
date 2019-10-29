085e33c699660668fbbf8342ef218d9855883ca4
Hoa <danghuyhoa@gmail.com>
2019-10-29 22:21:54 +0700

    [new] add api for get all todos in a category

--- src/main/java/my/app/has/todolist/repositories/ITodoRepository.java
+++ src/main/java/my/app/has/todolist/repositories/ITodoRepository.java
@@ -1,8 +1,9 @@
 package my.app.has.todolist.repositories;
 
+import my.app.has.todolist.models.Category;
 import my.app.has.todolist.models.Todo;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface ITodoRepository extends CrudRepository<Todo, Long> {
+    Iterable<Todo> findAllByCategory(Category category);
 }
--- src/main/java/my/app/has/todolist/services/ITodoService.java
+++ src/main/java/my/app/has/todolist/services/ITodoService.java
@@ -1,11 +1,13 @@
 package my.app.has.todolist.services;
 
+import my.app.has.todolist.models.Category;
 import my.app.has.todolist.models.Todo;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ITodoService {
     Iterable<Todo> getTodoList();
+    Iterable<Todo> getTodoByCategory(Category category);
     Todo getTodoDetail(Long id);
     Todo save(Todo todo);
 }

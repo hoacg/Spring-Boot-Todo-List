23e537e2b90547f42b60ffedcd265ffd69eba4dd
Hoa <danghuyhoa@gmail.com>
2019-11-02 09:29:41 +0700

    [improve] remove unused import lines

--- src/main/java/my/app/has/todolist/controllers/TodoController.java
+++ src/main/java/my/app/has/todolist/controllers/TodoController.java
@@ -4,14 +4,12 @@ import my.app.has.todolist.models.Tag;
 import my.app.has.todolist.models.Todo;
 import my.app.has.todolist.services.ITagService;
 import my.app.has.todolist.services.ITodoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController

43879c185a27c3514e7471d4aebcaae332e26116
Hoa <danghuyhoa@gmail.com>
2019-12-14 23:47:52 +0700

    [test] added test cases for testing ProductService

--- src/test/java/my/app/has/products/services/ProductServiceTest.java
+++ src/test/java/my/app/has/products/services/ProductServiceTest.java
@@ -1,57 +1,65 @@
 package my.app.has.products.services;
 
-import my.app.has.MyApplication;
 import my.app.has.products.models.Product;
 import my.app.has.products.repository.ProductRepository;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MyApplication.class)
+@RunWith(SpringRunner.class)
 class ProductServiceTest {
 
-    @MockBean
-    ProductRepository productRepository;
+    private ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+    private IProductService productService = new ProductService(productRepository);
 
-    @MockBean
-    IProductService productService;
-
-    @Before
+    @BeforeEach
     void init() {
         Product product = new Product();
         product.setName("iPhone X");
         product.setPrice(15000000d);
         product.setDescription("An Apple product");
-        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        // when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        doReturn(Optional.of(product)).when(productRepository).findById(1L);
 
-        List<Product> productList = new ArrayList<>();
-        productList.add(product);
-        when(productRepository.findAll()).thenReturn(productList);
-
-        productService = new ProductService(productRepository);
+        List<Product> productList = Arrays.asList(product);
+        // when(productRepository.findAll()).thenReturn(productList);
+        doReturn(productList).when(productRepository).findAll();
     }
 
+    @DisplayName("getList() can return list (is not null)")
     @Test
-    void getList() {
+    void whenGetList_thenNotNull() {
         assertThat(productService.getList()).isNotNull();
     }
 
-//    @Test
-//    void save() {
-//    }
-//
-//    @Test
-//    void delete() {
-//    }
+    @DisplayName("getList() return a list with 1 element")
+    @Test
+    public void whenFindAll_thenReturnProductList() {
+        // when
+        List<Product> actualProducts = productService.getList();
+
+        // then
+        assertThat(actualProducts).hasSize(1);
+    }
+
+    @DisplayName("findById() return a product has name `iPhone X`")
+    @Test
+    public void whenFindById_thenReturnProduct() {
+        // when
+        Optional<Product> product = productService.findById(1L);
+
+        // then
+        assertThat(product.get().getName()).isEqualTo("iPhone X");
+    }
+
 }

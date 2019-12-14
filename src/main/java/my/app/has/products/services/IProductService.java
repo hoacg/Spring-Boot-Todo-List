be1b6e7db5f3001d0104f50c2fccfd42b6a3f8ba
Hoa <danghuyhoa@gmail.com>
2019-12-14 23:47:09 +0700

    [new] implement findById method

--- src/main/java/my/app/has/products/services/IProductService.java
+++ src/main/java/my/app/has/products/services/IProductService.java
@@ -1,9 +1,12 @@
 package my.app.has.products.services;
 
 import my.app.has.products.models.Product;
+import java.util.List;
+import java.util.Optional;
 
 public interface IProductService {
-    Iterable<Product> getList();
+    Optional<Product> findById(Long id);
+    List<Product> getList();
     Product save(Product product);
     void delete(Long id);
 }
--- src/main/java/my/app/has/products/services/ProductService.java
+++ src/main/java/my/app/has/products/services/ProductService.java
@@ -5,6 +5,9 @@ import my.app.has.products.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService implements IProductService {
     private ProductRepository productRepository;
@@ -12,8 +15,14 @@ public class ProductService implements IProductService {
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
     @Override
-    public Iterable<Product> getList() {
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public List<Product> getList() {
         return productRepository.findAll();
     }
 

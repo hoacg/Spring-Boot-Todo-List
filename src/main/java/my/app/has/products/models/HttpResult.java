4f4c98d24941beabd7cbcdf9ce97af56eb8dace6
Hoa <danghuyhoa@gmail.com>
2020-02-01 20:23:00 +0700

    ignore data field in json if it's null

--- src/main/java/my/app/has/products/models/HttpResult.java
+++ src/main/java/my/app/has/products/models/HttpResult.java
@@ -1,5 +1,6 @@
 package my.app.has.products.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -8,5 +9,6 @@ import lombok.RequiredArgsConstructor;
 public class HttpResult<E> {
     private boolean success;
     private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private E data;
 }
